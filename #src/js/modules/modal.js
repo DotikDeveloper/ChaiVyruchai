e987b50d98341d203aef388127ecbe46e0140795
Dotik <36793535+DotikDeveloper@users.noreply.github.com>
2020-11-13 12:43:27 +0400

    add footer, homepage, and link documents

---
+++ #src/js/modules/modal.js
@@ -0,0 +1,41 @@
+function modal() {
+
+    const modalBg = document.querySelector(".overlay"),
+        logoutBtn = document.querySelector("button[data-button=close-modal]"),
+        loginBtn = document.querySelector("button[data-button=login]"),
+        inputModal = document.querySelectorAll("input");
+
+    logoutBtn.addEventListener("click", () => {
+        modalBg.classList.add("hide");
+        //  reset form__password
+        inputModal.forEach((i) => {
+            if (true) {
+                i.value = '';
+            }
+        });
+    });
+
+    loginBtn.addEventListener("click", () => {
+        const modalPage = document.querySelector('.overlay');
+
+        console.log("modal", modalPage);
+        modalPage.classList.remove("hide");
+
+    });
+
+    const modalPassword = document.querySelector("#form__password"),
+        btnShowPasswordImg = document.querySelector(".password-control img");
+
+    btnShowPasswordImg.addEventListener("click", () => {
+        if (modalPassword.getAttribute("type") === "password") {
+            modalPassword.setAttribute("type", "text");
+            btnShowPasswordImg.setAttribute("src", "img/modal/eye-open.svg");
+        } else {
+            modalPassword.setAttribute("type", "password");
+            btnShowPasswordImg.setAttribute("src", "img/modal/eye-close.svg");
+        }
+    });
+
+}
+
+export default modal;
--- #src/js/script.js
+++ #src/js/script.js
@@ -1,8 +1,12 @@
 'use string';
 
 import date from './modules/date';
+import slides from './modules/slides';
+import modal from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
     
     date('.footer__date');
+    slides();
+    modal();
 });
